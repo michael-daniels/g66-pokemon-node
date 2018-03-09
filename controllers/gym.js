0acd27c200998a24a5e4de409e39876c2a802492
‘Michael <michael.daniels.jr@gmail.com>
2018-03-08 19:00:08 -0700

    saving progress

--- controllers/gym.js
+++ controllers/gym.js
@@ -3,10 +3,28 @@ var knex = require('../db/knex');
 module.exports = {
 
   main: function(req, res, next) {
-    knex('pokemon').where('in_gym', '<>', 'true').then((results) => {
-      res.render('gym', {allpokemon:results});
+    let in_gym_true = [];
+    let in_gym_false = [];
+    knex('pokemon').then((results) => {
+      for (let i = 0; i < results.length; i++) {
+        if (results[i].in_gym == true) {
+          in_gym_true.push(results[i])
+        } else {
+          in_gym_false.push(results[i])
+        }
+
+      }
+      console.log('in gym true: ', in_gym_true);
+      console.log('in gym false: ', in_gym_false);
+      res.render('gym', {in_gym_false:in_gym_false, in_gym_true:in_gym_true});
     })
   },
+
+  // main: function(req, res, next) {
+  //   knex('pokemon').where('in_gym', '<>', 'true').then((results) => {
+  //     res.render('gym', {allpokemon:results});
+  //   })
+  // },
 
   add_gym: function(req, res, next) {
     knex('pokemon').where('id', req.body.id).update({in_gym: true})
