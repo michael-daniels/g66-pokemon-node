2d995e01414c77ac142a79346901455cc23dd16c
‘Michael <michael.daniels.jr@gmail.com>
2018-03-09 09:30:40 -0700

    images added to migration and seed

--- db/migrations/20180308094828_pokemon.js
+++ db/migrations/20180308094828_pokemon.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('trainers',(table)=>{
     table.increments();
     table.string('trainer_name');
+    table.string('img_url');
   })
 };
 
